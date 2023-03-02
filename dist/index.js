27d8e6a06426f7d6f2352c8b1598296191e6f2b9
Fabrizio Nastri <fabrizio.nastri@flexup.org>
2023-03-02 22:22:42 +0100

    First working ToDo List

--- dist/index.js
+++ dist/index.js
@@ -1,54 +1,113 @@
 "use strict";
 const btn = document.getElementById("btn"); // type assertion
-const input = document.getElementById("todoinput");
+const input = document.getElementById("input");
 const form = document.querySelector("form");
-const todolist = document.getElementById("todolist");
+const list = document.getElementById("list");
+function loadToDos() {
+    const toDosJSON = localStorage.getItem("toDos");
+    if (!toDosJSON)
+        return [];
+    return JSON.parse(toDosJSON);
+}
+// display the toDos array on page load
+const toDos = loadToDos(); // load the toDos array from local storage
+toDos.forEach(createToDoElement); // add the toDos to the ul element (the list variable
+// toDos.forEach((toDo: ToDo) => { // this is the same as the line above
+//   createToDoElement(toDo);
+// });
+function saveToDos() {
+    localStorage.setItem("toDos", JSON.stringify(toDos)); // convert the toDos array to a JSON string and save it to local storage under the key "toDos"
+}
 function addToDo(event) {
+    console.log("Event: ", event); // print the event object to the console
     event.preventDefault(); // prevent the default behaviour of the form, which is to refresh the page
-    const newToDo = input.value; // get the value of the input
-    console.log("New toDo submited: ", newToDo);
-    const newElement = document.createElement("li"); // create a new li element
-    newElement.innerText = newToDo; // set the text of the new li element to the value of the input
-    //create a check box and prepend it to the new li element
-    const checkBox = document.createElement("input");
+    const toDo = {
+        text: input.value,
+        done: false,
+    }; // assign the value of the input to the newToDo variable
+    createToDoElement(toDo);
+    toDos.push(toDo); // add the new toDo to the toDos array
+    saveToDos(); // save the toDos array to local storage
+    input.value = ""; // clear the input field
+}
+function createToDoElement(toDo) {
+    console.log('list object: ', list)
+    console.log('toDo object: ', toDo)
+    const toDoBlock = document.createElement("li"); // create a new li element
+    list.appendChild(toDoBlock); // append the new li element to the ul element
+    const checkBox = document.createElement("input"); //create a check box and prepend it to the new li element
     checkBox.type = "checkbox";
-    newElement.prepend(checkBox);
+    toDoBlock.appendChild(checkBox);
+    const toDoText = document.createElement("span"); //create a span element and append it to the new li element
+    toDoText.innerText = toDo.text; // set the text of the span element to the text of the toDo object
+    toDoBlock.appendChild(toDoText);
+    const actionDiv = document.createElement("div"); //create a div and append it to the new li element
+    actionDiv.className = "float-right"; // give the div a class name
+    toDoBlock.appendChild(actionDiv); // append the delete button to the new li element
+    // create up, down and delete buttons and append them to the new actionDiv
+    const upButton = document.createElement("button");
+    upButton.className = "text-button";
+    upButton.innerText = "△";
+    actionDiv.appendChild(upButton);
+    const downButton = document.createElement("button");
+    downButton.className = "text-button";
+    downButton.innerText = "▽";
+    actionDiv.appendChild(downButton);
     const deleteButton = document.createElement("button"); //create a delete button and append it to the new li element
-    deleteButton.style.border = "none"; // make the delete button have no border and no background
-    deleteButton.style.background = "none";
+    deleteButton.className = "text-button"; // give the delete button a class name
     deleteButton.innerText = "✕"; // set the text of the delete button to the unicode character for the cross symbol
-    deleteButton.style.float = "right"; // align the delete button to the right of the container
-    newElement.appendChild(deleteButton); // append the delete button to the new li element
-    todolist.appendChild(newElement); // append the new li element to the ul element
+    actionDiv.appendChild(deleteButton);
 }
-// a function that changes the background color of the body element to a random color when called
-function changeBackground(event) {
-    event.preventDefault(); // prevent the default behaviour of the form, which is to refresh the page
-    // generate a random color
-    const randomColor = generateLightColor();
-    // generate a light random color
-    document.body.style.backgroundColor = "#" + randomColor;
-}
-// form.addEventListener("submit", changeBackground)  // we pass the callback function to the event listener, without any arguments, because the event object is passed to the callback function by the browser automatically
-// btn.addEventListener("click", addToDo)
-
-form.addEventListener("submit", (e) => { // we pass the callback function to the event listener, without any arguments, because the event object is passed to the callback function by the browser automatically
-    addToDo(e);
-    changeBackground(e);
-});
-
-
-
-todolist.addEventListener("click", (e) => { // if a deleteButton is clicked, remove the parent element of the deleteButton
-    if (e.target.tagName === "BUTTON") { // a tagName is the name of the html element, e.g. button, li, ul, etc.
-        e.target.parentElement.remove(); // remove the parent element of the deleteButton
+form.addEventListener("submit", addToDo); // when I click on the submit button or hit enter in the input field, I want to add a new ToDo
+list.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target.type === "checkbox") {
+        const parent = target.parentNode;
+        parent.style.textDecoration = target.checked ? "line-through" : "none";
     }
 });
-
-
-todolist.addEventListener("click", (e) => { // if a checkbox is clicked, add a line-through style to the parent element of the checkbox
-    if (e.target.tagName === "INPUT") {
-        e.target.parentElement.style.textDecoration = "line-through";
+list.addEventListener("click", (event) => {
+    const button = event.target; // button is target element that was clicked
+    const line = button.parentNode.parentNode; // line is the li element that contains the button (the parent of the parent of the button)
+    const span = line.querySelector("span"); // span is the element that contains the text of the toDo
+    const index = toDos.findIndex((toDo) => toDo.text == span.innerText); // find the index of the toDo object in the toDos array
+    console.log("ToDos before taking any action: ", toDos);
+    console.log("Target toDo index :", index, " = ", span.innerText); // print the value of the toDo object to the console
+    switch (button.innerText) { // switch statement to check the text of the button
+        case "△": {
+            const previousLine = line.previousElementSibling; // the previousElement is the previous li element
+            if (previousLine) { // if the previousElement exists, move the li element up the list
+                list.insertBefore(line, previousLine); // insert the li element before the previous li element
+                const previousIndex = index - 1; // find the index of the previous toDo object in the toDos array
+                console.log("Previous index: ", previousIndex);
+                const temp = toDos[previousIndex]; // create a temporary variable to store the toDo object
+                toDos[previousIndex] = toDos[index]; // assign the toDo text of the previous toDo object to the current toDo object
+                toDos[index] = temp; // assign the toDo text of the current toDo object to the previous toDo object
+                console.log("ToDos after moving up: ", toDos);
+            }
+            break;
+        }
+        case "▽": {
+            const nextLine = line.nextElementSibling; // the nextElement is the next li element
+            if (nextLine) { // if the nextElement exists, move the li element down the list
+                list.insertBefore(nextLine, line); // insert the li element before the next li element
+                const nextIndex = index + 1; // find the index of the next toDo object in the toDos array
+                console.log("Next index: ", nextIndex);
+                const temp = toDos[nextIndex]; // create a temporary variable to store the toDo object
+                toDos[nextIndex] = toDos[index]; // assign the toDo text of the next toDo object to the current toDo object
+                toDos[index] = temp; // assign the toDo text of the current toDo object to the next toDo object
+                console.log("ToDos after moving down: ", toDos);
+            }
+            break;
+        }
+        case "✕": {
+            // delete the line from the ul element and the toDo object from the toDos array
+            list.removeChild(line);
+            toDos.splice(index, 1);
+            console.log("ToDos after deleting: ", toDos);
+            break;
+        }
+        default: break;
     }
+    saveToDos(); // update the local storage
 });
-
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,130 @@
-const btn = document.getElementById("btn")! as HTMLButtonElement; // type assertion
-const input = document.getElementById("todoinput")! as HTMLInputElement;
-const form = document.querySelector("form")!
-const todolist = document.getElementById("todolist")! as HTMLUListElement;
+interface ToDo {
+  text: string;
+  done: boolean ;
+}
 
+const btn = document.getElementById("btn")! as HTMLButtonElement; // type assertion
+const input = document.getElementById("toDoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!
+const list = document.getElementById("list")! as HTMLUListElement;
+
+
+function loadToDos(): ToDo[] { // on page load, get the toDos array from local storage,  add them to the ul element and add them to the toDos array
+  const toDosJSON = localStorage.getItem("toDos")
+  if (toDosJSON === null) return [];
+  return JSON.parse(toDosJSON);
+}
+
+// display the toDos array on page load
+const toDos: ToDo[] = loadToDos(); // load the toDos array from local storage
+toDos.forEach(createToDoElement); // add the toDos to the ul element (the list variable
+// toDos.forEach((toDo: ToDo) => { // this is the same as the line above
+//   createToDoElement(toDo);
+// });
+ 
+
+
+function saveToDos() { // save the toDos array to local storage
+  localStorage.setItem("toDos", JSON.stringify(toDos)); // convert the toDos array to a JSON string and save it to local storage under the key "toDos"
+}
 
 
 function addToDo(event : Event) { // event is the event object, which is passed to the callback function by the browser when the event is triggered (in this case, the submit event) 
+  console.log("Event: ", event); // print the event object to the console
   event.preventDefault(); // prevent the default behaviour of the form, which is to refresh the page
-  const newToDo = input.value; // get the value of the input
-  console.log("New toDo submited: ", newToDo);
-  const newElement = document.createElement("li"); // create a new li element
-  newElement.innerText = newToDo; // set the text of the new li element to the value of the input
-  //create a check box and prepend it to the new li element
-  const checkBox = document.createElement("input");
+  const toDo: ToDo = { 
+    text: input.value, 
+    done: false, 
+   }; // assign the value of the input to the newToDo variable
+  createToDoElement(toDo);
+  toDos.push(toDo); // add the new toDo to the toDos array
+  saveToDos() // save the toDos array to local storage
+  input.value = ""; // clear the input field
+}
+ 
+function createToDoElement(toDo: ToDo) {
+  const toDoBlock = document.createElement("li"); // create a new li element
+  list.appendChild(toDoBlock); // append the new li element to the ul element
+
+  const checkBox = document.createElement("input");  //create a check box and prepend it to the new li element
   checkBox.type = "checkbox";
-  newElement.prepend(checkBox);
+  toDoBlock.appendChild(checkBox);
+
+  const toDoText = document.createElement("span"); //create a span element and append it to the new li element
+  toDoText.innerText = toDo.text; // set the text of the span element to the text of the toDo object
+  toDoBlock.appendChild(toDoText);
+
+  const actionDiv = document.createElement("div"); //create a div and append it to the new li element
+  actionDiv.className = "float-right"; // give the div a class name
+  toDoBlock.appendChild(actionDiv); // append the delete button to the new li element
+
+  // create up, down and delete buttons and append them to the new actionDiv
+  const upButton = document.createElement("button");   
+  upButton.className = "text-button"; 
+  upButton.innerText = "△"; 
+  actionDiv.appendChild(upButton);
+
+  const downButton = document.createElement("button");   
+  downButton.className = "text-button"; 
+  downButton.innerText = "▽"; 
+  actionDiv.appendChild(downButton); 
+
   const deleteButton = document.createElement("button");   //create a delete button and append it to the new li element
-  deleteButton.style.border = "none";  // make the delete button have no border and no background
-  deleteButton.style.background = "none";
+  deleteButton.className = "text-button"; // give the delete button a class name
   deleteButton.innerText = "✕"; // set the text of the delete button to the unicode character for the cross symbol
-  deleteButton.style.float = "right"; // align the delete button to the right of the container
-  newElement.appendChild(deleteButton); // append the delete button to the new li element
-  todolist.appendChild(newElement); // append the new li element to the ul element
+  actionDiv.appendChild(deleteButton); 
+
 }
 
-// a function that changes the background color of the body element to a random color when called
-function changeBackground(event : Event) {
-  event.preventDefault(); // prevent the default behaviour of the form, which is to refresh the page
-  // generate a random color
-  const randomColor = generateLightColor()
-  // generate a light random color
-  document.body.style.backgroundColor = "#" + randomColor;
-}
+form.addEventListener("submit", addToDo) // when I click on the submit button or hit enter in the input field, I want to add a new ToDo
 
-// form.addEventListener("submit", changeBackground)  // we pass the callback function to the event listener, without any arguments, because the event object is passed to the callback function by the browser automatically
-// btn.addEventListener("click", addToDo)
+list.addEventListener("click", (event) => { // when I click on a checkbox, I want to add a line-through to the text of the li element
+  const target = event.target as HTMLInputElement;
+  if (target.type === "checkbox") {
+    const parent = target.parentNode as HTMLLIElement;
+    parent.style.textDecoration = target.checked ? "line-through" : "none";
+}})
 
-form.addEventListener("submit", (e) => {
-  addToDo(e);
-  changeBackground(e);
- })
-
+list.addEventListener("click", (event) => { // action buttons (up, down and delete)
+  const button = event.target as HTMLButtonElement; // button is target element that was clicked
+  const line = button.parentNode!.parentNode as HTMLLIElement; // line is the li element that contains the button (the parent of the parent of the button)
+  const span = line.querySelector("span")! as HTMLSpanElement; // span is the element that contains the text of the toDo
+  const index = toDos.findIndex((toDo: ToDo) => toDo.text == span.innerText); // find the index of the toDo object in the toDos array
+  console.log("ToDos before taking any action: ", toDos);
+  console.log("Target toDo index :", index, " = ", span.innerText)     // print the value of the toDo object to the console
+  switch (button.innerText) { // switch statement to check the text of the button
+    case "△":  {
+        const previousLine = line.previousElementSibling as HTMLLIElement; // the previousElement is the previous li element
+        if (previousLine) {  // if the previousElement exists, move the li element up the list
+          list.insertBefore(line, previousLine); // insert the li element before the previous li element
+          const previousIndex = index - 1; // find the index of the previous toDo object in the toDos array
+          console.log("Previous index: ", previousIndex)
+          const temp = toDos[previousIndex]; // create a temporary variable to store the toDo object
+          toDos[previousIndex] = toDos[index]; // assign the toDo text of the previous toDo object to the current toDo object
+          toDos[index] = temp; // assign the toDo text of the current toDo object to the previous toDo object
+          console.log("ToDos after moving up: ", toDos);   
+        } break;
+    }
+    case "▽":  {
+      const nextLine = line.nextElementSibling as HTMLLIElement; // the nextElement is the next li element
+      if (nextLine) {  // if the nextElement exists, move the li element down the list
+        list.insertBefore(nextLine, line); // insert the li element before the next li element
+        const nextIndex = index + 1; // find the index of the next toDo object in the toDos array
+        console.log("Next index: ", nextIndex)
+        const temp = toDos[nextIndex]; // create a temporary variable to store the toDo object
+        toDos[nextIndex] = toDos[index]; // assign the toDo text of the next toDo object to the current toDo object
+        toDos[index] = temp; // assign the toDo text of the current toDo object to the next toDo object
+        console.log("ToDos after moving down: ", toDos);
+      }  break;
+    }
+    case "✕":  {
+      // delete the line from the ul element and the toDo object from the toDos array
+      list.removeChild(line);
+      toDos.splice(index, 1);
+      console.log("ToDos after deleting: ", toDos);
+      break;      
+    }
+    default:  break;
+  }
+  saveToDos() // update the local storage
+})
